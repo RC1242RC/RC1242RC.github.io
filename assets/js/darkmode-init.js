e26153a246207dd68193138dc0001e7c81a6c3a9
RC1242RC <r.clark1242@gmail.com>
2022-07-27 10:40:26 +0100

    Create empty gh-pages

--- assets/js/darkmode-init.js
+++
@@ -1,7 +0,0 @@
-const localMode = localStorage.getItem('theme');
-
-if (localMode === 'dark') {
-
-  document.documentElement.setAttribute('data-dark-mode', '');
-
-}
--- content/en/community/_index.md
+++
@@ -1,8 +0,0 @@
----
-title: "Community"
-description: ""
-date: 2022-07-01T22:31:22+01:00
-lastmod: 2022-07-01T22:31:22+01:00
-draft: false
-images: []
----
